cc2e8f91cd997e242b303fb6d6732c1f8408accb
Rahul Kamti <kamti03rahul@gmail.com>
2024-11-07 22:42:00 +0530

    Update 21_UpperCase_Or_LowerCase.c

--- 21_UpperCase_Or_LowerCase.c
+++ 21_UpperCase_Or_LowerCase.c
@@ -8,9 +8,9 @@ int main() {
 
     if (ch >= 'A' && ch <= 'Z') {
         printf("The character is an uppercase alphabet.\n");
-    else if (ch >= 'a' && ch <= 'z') {
+    } else if (ch >= 'a' && ch <= 'z') {
         printf("The character is a lowercase alphabet.\n");
-    else {
+    } else {
         printf("The character is not an alphabet.\n");
     }
 
